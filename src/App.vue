3bb9b1a985d7af5deeb47fc162bf6211a280aa52
Maël Billaud <92854452+maelBillaud@users.noreply.github.com>
2022-10-21 15:04:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Map />
+  </div>
+</template>
+
+<script>
+import Map from "./components/map.vue";
+
+export default {
+  name: "App",
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/map.vue
@@ -0,0 +1,49 @@
+<template>
+  <l-map
+    :center="center"
+    :zoom="zoom"
+    class="map"
+    ref="map"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+  >
+    <l-tile-layer :url="url"> </l-tile-layer>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      center: [49.1193089, 6.1757156],
+      zoom: 12,
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+      console.log(this.markers);
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+  },
+};
+</script>
+
+<style>
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
